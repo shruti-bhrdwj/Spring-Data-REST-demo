7509dfe2d3005b65f69fe079c14edeeb6b7d0eba
Shruti <bhardwaj.shruti03@gmail.com>
2022-07-21 17:18:03 +0530

    first

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'shruti','Java');
+insert into alien values (102,'kanika','Java');
+insert into alien values (103,'Jaanvi','Python');
+insert into alien values (104,'Sakshi','ML');
+insert into alien values (105,'Dipika','Biology');
